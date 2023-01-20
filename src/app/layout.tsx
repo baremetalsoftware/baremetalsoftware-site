2befb13b256bb0a977191baaed3ca08af1cee1bc
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2023-01-20 09:42:18 +0000

    add basic page structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">
+        <header className="flex justify-center">
+          Header
+          <nav>Nav</nav>
+        </header>
+        <main className="flex flex-1 flex-col justify-between items-center">{children}</main>
+        <footer className="flex justify-center">Footer</footer>
+      </body>
     </html>
   );
 }
